72f368f1a5e6523dbe75c82ef7683c296453a81a
BrockBadeaux14 <bbadea9@lsu.edu>
2025-02-18 18:28:07 -0600

    (hotfix) Makes it to where CastLure actually accounts for powerBarValue

--- Assets/Scripts/CastState.cs
+++ Assets/Scripts/CastState.cs
@@ -2,27 +2,34 @@ using UnityEngine;
 
 public class CastState : StateInterface
 {
-    private readonly float castSpeed;
+    private float castSpeed;
+    private float maxCastSpeed;
     private readonly GameObject lurePrefab;
     private readonly Transform castOrigin;
     private readonly DirectionIndicator directionIndicator;
     private GameObject currentLure;
+    private CastingMinigame castingMinigame;
+    
 
     public CastState(
         float castSpeed,
+        float maxCastSpeed,
         GameObject lurePrefab,
         Transform castOrigin,
-        DirectionIndicator directionIndicator)
+        DirectionIndicator directionIndicator,
+        CastingMinigame castingMinigame)
     {
         this.castSpeed = castSpeed;
+        this.maxCastSpeed = maxCastSpeed;
         this.lurePrefab = lurePrefab;
         this.castOrigin = castOrigin;
         this.directionIndicator = directionIndicator;
+        this.castingMinigame = castingMinigame;
     }
 
     public void Enter()
     {
-        // Initialize state if needed
+        castingMinigame.enabled = true;
     }
 
     public void Update()
@@ -35,15 +42,15 @@ public class CastState : StateInterface
         Vector2 castDirection = directionIndicator.getCurrentDirection();
         CameraController cameraController = Camera.main.GetComponent<CameraController>();
         CastLure(castDirection);
-        cameraController.target = currentLure.transform;
-        
-        
+        cameraController.target = currentLure.transform;  
     }
 
     private void CastLure(Vector2 castDirection)
     {
         currentLure = Object.Instantiate(lurePrefab, castOrigin.position, Quaternion.identity);
         Rigidbody2D rb = currentLure.GetComponent<Rigidbody2D>();
+        castSpeed = maxCastSpeed * (castingMinigame.getCurrentPowerBarValue()/100);
+        //Debug.Log($"Casting with speed {castSpeed}");
         rb.AddForce(castDirection * castSpeed, ForceMode2D.Impulse);
     }
 }

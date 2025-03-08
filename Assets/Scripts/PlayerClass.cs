bf86705a14dabd3b06ead78462ef0eb77eca1f7d
Jatki-Project-Submission <163676103+Jatki-Project-Submission@users.noreply.github.com>
2025-03-08 15:37:45 -0600

    Player Class monoscript
    added a player class with name, might, dexterity, magica, movespeed, exp points, and gold variables.

---
+++ Assets/Scripts/PlayerClass.cs
@@ -0,0 +1,13 @@
+using UnityEngine;
+
+public class PlayerClass: MonoBehaviour
+{
+    public string Name { get; set; }
+    public int might { get; set; }
+    public int dexterity { get; set; }
+    public int magica { get; set; }
+    public float moveSpeed { get; set; }
+    public int experiencePoints {  get; set; }
+    public int gold {  get; set; }
+    
+}

2946dd91f99fbd542a8281eee31899138a550aa4
BrockBadeaux14 <bbadea9@lsu.edu>
2025-03-29 19:04:14 -0500

    Creation of SerializableFishItem
    This is to store within inventory

---
+++ Assets/Scripts/Item scripts/SerializableFishItem.cs
@@ -0,0 +1,24 @@
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+
+[System.Serializable]
+public class SerializableFishItem
+{
+    // Basic properties
+    public string fishName;
+    public string description;
+    public Sprite icon;
+    public FishType baseFishType;
+    public Rarity rarity;
+
+    // Modifier properties
+    public float speedMultiplier = 1f;
+    public float sizeMultiplier = 1f;
+    public float forceMultiplier = 1f;
+    public float expValue = 100f;
+    public float gearDropChance = 0.5f;
+    public int gearRarityBonus = 0;
+    public string glowEffect = "";
+    public int explicitModCount = 0;
+}

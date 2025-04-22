bd5eee46548eefd9b53aea78a6a3c0e9fdb7e71a
vekovius <93623258+vekovius@users.noreply.github.com>
2025-04-22 03:22:11 -0500

    Delete the lure
    trigger event to destroy line once lure is destroyed.

--- Assets/Scripts/Misc/FishingLineDrawer.cs
+++ Assets/Scripts/Misc/FishingLineDrawer.cs
@@ -9,22 +9,36 @@ public class FishingLineDrawer : MonoBehaviour
     private void OnEnable()
     {
         Lure.OnLureCreated += SetLureTarget;
+        Lure.OnLureDestroyed += ClearLureTarget;
     }
 
     private void OnDisable()
     {
         Lure.OnLureCreated -= SetLureTarget;
+        Lure.OnLureDestroyed -= ClearLureTarget;
     }
 
     private void Start()
     {
         lineRenderer = GetComponent<LineRenderer>();
+        // Hide line initially
+        if (lineRenderer != null && lureTransform == null)
+        {
+            lineRenderer.enabled = false;
+        }
     }
 
     private void Update()
     {
-        if (lureTransform != null)
+        if (lureTransform != null && lineRenderer != null)
         {
+            // Show line when lure exists
+            if (!lineRenderer.enabled)
+            {
+                lineRenderer.enabled = true;
+            }
+
+            // Update line positions
             lineRenderer.SetPosition(0, playerTransform.position);
             lineRenderer.SetPosition(1, lureTransform.position);
         }
@@ -33,5 +47,16 @@ public class FishingLineDrawer : MonoBehaviour
     void SetLureTarget(Transform lure)
     {
         lureTransform = lure;
+    }
+    
+    void ClearLureTarget()
+    {
+        lureTransform = null;
+        
+        // Hide the line renderer when lure is destroyed
+        if (lineRenderer != null)
+        {
+            lineRenderer.enabled = false;
+        }
     }
 }

f5da0bbe75d216070227b32595b94da44d3d611f
vekovius <93623258+vekovius@users.noreply.github.com>
2025-05-01 19:33:25 -0500

    Skill script in 100%
    Skill tree actually being hooked up now, about 25% of the way done. The actual uses of these skills need to be implemented too but all the framework is done

--- Assets/SkillTree.cs
+++ Assets/SkillTree.cs
@@ -1,12 +1,21 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
 public class SkillTree : MonoBehaviour
 {
+    public bool useNextSkill = false;
+    public GameObject nextSkill;
+    public bool unlocked = false;
+
+    public string aboutSkill;
+
     public float cost;
     public GameObject mask;
 
     public GameObject confirmationPanel;
+    public TextMeshProUGUI text;
+
     private Button yesButton;
     private Button noButton;
 
@@ -14,42 +23,73 @@ public class SkillTree : MonoBehaviour
 
     public void ClickSkill() 
     {
-        if (!bought)
+        if (unlocked)
         {
-            confirmationPanel.SetActive(true);
-            //add in switches text to show about skill
+            if (!bought)
+            {
+                confirmationPanel.SetActive(true);
+                //add in switches text to show about skill
 
-            yesButton = GameObject.FindGameObjectWithTag("YesButton").GetComponent<Button>();
-            noButton = GameObject.FindGameObjectWithTag("NoButton").GetComponent<Button>();
+                yesButton = GameObject.FindGameObjectWithTag("YesButton").GetComponent<Button>();
+                noButton = GameObject.FindGameObjectWithTag("NoButton").GetComponent<Button>();
+                text.text = aboutSkill;
 
-            yesButton.onClick.AddListener(BuySkill);
-            noButton.onClick.AddListener(Exit);
+                yesButton.onClick.AddListener(BuySkill);
+                noButton.onClick.AddListener(Exit);
 
-            Debug.Log("Opened Confirmation Panel");
+                Debug.Log("Opened Confirmation Panel");
+            }
+            else
+            {
+                confirmationPanel.SetActive(true);
+
+                yesButton.gameObject.SetActive(false);
+                noButton.onClick.AddListener(Exit);
+                text.text = aboutSkill;
+
+                Debug.Log("Show about skill");
+            }
         }
         else 
         {
-            Debug.Log("Show about skill");
+             confirmationPanel.SetActive(true);
+            yesButton = GameObject.FindGameObjectWithTag("YesButton").GetComponent<Button>();
+            noButton = GameObject.FindGameObjectWithTag("NoButton").GetComponent<Button>();
+
+
+            yesButton.gameObject.SetActive(false);
+             noButton.onClick.AddListener(Exit);
+             text.text = aboutSkill;
+
+             Debug.Log("Show about skill");
         }
     }
 
     public void BuySkill() 
     {
-
         //if have money then skill
         mask.SetActive(false);
+
+        if(useNextSkill) 
+        {
+            nextSkill.GetComponent<SkillTree>().unlocked = true;
+        }
 
         Debug.Log("Bought skill");
         bought = true;
         yesButton.onClick.RemoveListener(BuySkill);
         noButton.onClick.RemoveListener(Exit);
         confirmationPanel.SetActive(false);
+
+        //else 
+        //Exit();
     }
 
     public void Exit() 
     {
         Debug.Log("Exited skill");
 
+        yesButton.gameObject.SetActive(true);
         yesButton.onClick.RemoveListener(BuySkill);
         noButton.onClick.RemoveListener(Exit);
         confirmationPanel.SetActive(false);
